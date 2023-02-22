8ed37cd30397d1b284e4d23298354ec4eaf116a3
antdoan123 <aqdoan@cpp.edu>
2023-02-22 04:19:05 -0800

    now??

--- pages/components/MenuLink.jsx
+++ pages/components/MenuLink.jsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 
 const MenuLink = ({ pageName, link }) => {
   return (
-    <Menu.Item>
-      {({ active }) => (
-        <Link href={`${link}`}>
-          <button className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-violet-500 hover:text-white`}>
-            {pageName}
-          </button>
-        </Link>
-      )}
-    </Menu.Item>
+    <Menu>
+        <Menu.Item>
+        {({ active }) => (
+            <Link href={`${link}`}>
+            <button className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-violet-500 hover:text-white`}>
+                {pageName}
+            </button>
+            </Link>
+        )}
+        </Menu.Item>
+    </Menu>
   );
 }
 
