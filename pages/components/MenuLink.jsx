e59f99418d2c8f830eb6e37d2f8fad3d9e40a8b1
antdoan123 <aqdoan@cpp.edu>
2023-02-22 04:17:19 -0800

    menu

--- pages/components/MenuLink.jsx
+++ pages/components/MenuLink.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import { Menu, Transition } from '@headlessui/react'
-import Link from 'next/link'
+import Link from 'next/link';
 
-function MenuLink(props) {
-    return(
-            <Menu.Item>
-                {({ active }) => (
-                <Link href={`${props.link}`}>
-                    <button className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-violet-500 hover:text-white`}>
-                        {props.pageName}
-                    </button>
-                </Link>
-                )}
-            </Menu.Item>
-    )
+const MenuLink = ({ pageName, link }) => {
+  return (
+    <Menu.Item>
+      {({ active }) => (
+        <Link href={`${link}`}>
+          <button className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-violet-500 hover:text-white`}>
+            {pageName}
+          </button>
+        </Link>
+      )}
+    </Menu.Item>
+  );
 }
 
 export default MenuLink;
