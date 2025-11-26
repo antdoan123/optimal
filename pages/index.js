52b246a0b2a77e0dac62f50d1c03d639e3fb9a02
antdoan123 <aqdoan@cpp.edu>
2025-11-25 18:05:05 -0800

    logo

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index,follow" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/optimallogo.png" />
 
         {/* Open Graph / Social preview */}
         <meta
